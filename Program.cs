6cbebfa24585434b15d6fa14c847639f564632e0
aditi519 <aditipawar510@gmail.com>
2023-07-16 12:58:06 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Linq
+{
+    static void Main(string[] args)
+    {
+        string[] dayWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+
+        Console.Write("\nDays of a week : \n");
+
+        var days = from WeekDay in dayWeek
+                   select WeekDay;
+
+        foreach (var WeekDay in days)
+        {
+            Console.WriteLine(WeekDay);
+        }
+        Console.WriteLine();
+    }
+}
